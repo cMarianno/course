fcda788d9632c625b30a5dcdf9b14ce2a45ff01e
Caio Mariano <caiosadao@gmail.com>
2023-12-27 15:22:48 -0300

    initial commit

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Course } from './courses.entity';
+
+@Injectable()
+export class CoursesService {
+	private courses: Course[] = [
+		{
+			id: 1,
+			name: 'NestJS',
+			description: 'Curso sobre fundamentos do framework NestJS',
+			tags: ['node.js', 'nestjs', 'javascript', 'typescript']
+		}
+	];
+
+	findAll() {
+		return this.courses;
+	}
+
+	findOne(id:number){
+		const course = this.courses.find(course => course.id === id);
+
+		if (!course){
+			throw new NotFoundException(`Course ID ${id} not found`);
+		}
+
+		return course;
+	}
+
+	create(createCourseDTO: any){
+		this.courses.push(createCourseDTO);
+
+		return createCourseDTO;
+	}
+
+	update(id:number, updateCourseDTO: any){
+		const existindCourse = this.findOne(id);
+		if (existindCourse) {
+			const index = this.courses.findIndex(course => course.id === id);
+
+			this.courses[index] = {
+				id,
+				...updateCourseDTO
+			};
+		}
+	}
+
+	remove(id:number){
+		const index = this.courses.findIndex(course => course.id === id);
+
+		if (index >= 0){
+			this.courses.splice(index, 1);
+		}
+	}
+}
